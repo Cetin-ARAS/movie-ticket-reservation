5228d3060d8446d3b3b7a53128ee5586f7f85133
ÇETİN <cetinnnarasss@gmail.com>
2022-12-21 13:22:05 +0300

    finish

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const container = document.querySelector(".container");
 const count = document.getElementById("count");
 const amount = document.getElementById("amount");
-const select = document.getElementById("movie")
-const seats = document.querySelectorAll(".seat:not(.reserved)")
+const select = document.getElementById("movie");
+const seats = document.querySelectorAll(".seat:not(.reserved)");
+
+getFromLocalStorage();
+calculateTotal();
 
 container.addEventListener('click',function(e) {
     if(e.target.classList.contains("seat") && !e.target.classList.contains("reserved")){
@@ -32,7 +35,7 @@ function calculateTotal(){
     let selectedSeatIndexs = selectedSeatsArr.map(function(seat){
         return seatsArr.indexOf(seat);
     }); 
-    console.log(selectedSeatIndexs);
+ 
 
 
     
@@ -40,4 +43,27 @@ function calculateTotal(){
     count.innerText = selectedSeatCount;
     amount.innerText = selectedSeatCount * select.value; 
 
+    saveToLocalStorage(selectedSeatIndexs)
+
+}
+function getFromLocalStorage(){
+    const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"))
+
+    if(selectedSeats != null && selectedSeats.length > 0){
+        seats.forEach(function(seat, index){
+            if (selectedSeats.indexOf(index) > -1){
+                seat.classList.add("selected")
+
+            }
+        });
+    }
+    const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+
+    if(selectedMovieIndex != null){
+        select.selectedIndex = selectedMovieIndex;
+    }
+}
+function saveToLocalStorage(index){
+    localStorage.setItem("selectedSeats", JSON.stringify(index))
+    localStorage.setItem("selectedMovieIndex", select.selectedIndex)
 }
